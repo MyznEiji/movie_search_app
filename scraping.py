963bcad1cbeebb7ab16d04da32178539fd7b713e
MyznEiji <em711th@icloud.com>
2020-03-08 14:20:11 +0000

    save data to mysql

--- scraping.py
+++ scraping.py
@@ -3,26 +3,65 @@ import urllib.request as req
 import json
 import requests
 
+import mysql.connector
+
+
 url = 'https://www.clickthecity.com/movies/now-showing.php'
-res = req.urlopen(url)
-soup = BeautifulSoup(res,"html.parser")
 
-urls = soup.find_all('h3', itemprop="name")
-for url in urls:
+
+
+def get_urls(url):
+
+    res = req.urlopen(url)
+    soup = BeautifulSoup(res,"html.parser")
+    urls = soup.find_all('h3', itemprop="name")
+
+    return urls
+
+
+def is_nulldata(data):
+    if not data:
+        return ""
+    else:
+        return data.string
+
+
+def get_data(url):
+    # get the link for each page
     url = url.find('a')['href']
-
     res = req.urlopen(url)
     soup = BeautifulSoup(res,"html.parser")
 
 
-
-    title = soup.find('div', id='subcontent').find('span', itemprop="name").string
-
-    date = soup.find('div', id='subcontent').find('span', class_="year").string
-
-    description = soup.find('div', itemprop='description').string
-
+    # get the data
+    title = is_nulldata(soup.find('div', id='subcontent').find('span', itemprop="name"))
+    date = is_nulldata(soup.find('div', id='subcontent').find('span', class_="year"))
+    description = is_nulldata(soup.find('div', id='details').find('div', itemprop='description'))
     poster = soup.find('div', id="poster").find('meta', itemprop="url")["content"]
 
+    return title, date, description, poster
 
 
+def save_data(title, date, description, poster):
+    # save on db
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='hoge',
+        password="password",
+        database='movie')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO movies(title, date, description, poster) values(%s,%s,%s,%s)" , (title, date, description, poster))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+
+if __name__ == '__main__':
+    # get the html
+
+    urls = get_urls(url)
+
+    for url in urls:
+        title, date, description, poster = get_data(url)
+        save_data(title, date, description, poster)
